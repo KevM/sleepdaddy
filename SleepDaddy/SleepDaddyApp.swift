d68d008e82db80ef2d674da8922d14c7dca717f6
Kevin Miller <kevin@fm.rodeo>
2026-08-05 07:03:46 -0500

    feat: register SleepDaddy as a CSV document handler

--- SleepDaddy/SleepDaddyApp.swift
+++ SleepDaddy/SleepDaddyApp.swift
@@ -17,6 +17,9 @@ struct SleepDaddyApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView(model: model)
+                .onOpenURL { url in
+                    Task { try? await model.importVitals(from: url) }
+                }
         }
     }
 }
